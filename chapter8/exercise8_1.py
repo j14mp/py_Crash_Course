7cfd07132893e909141c3412f2d0e81c8a17c2a0
j14mp <97001372+j14mp@users.noreply.github.com>
2022-01-06 22:11:59 -0800

    Create exercise8_1.py

---
+++ chapter8/exercise8_1.py
@@ -0,0 +1,9 @@
+def display_message():
+	print('We are learning about functions this chapter.')
+
+display_message()
+
+def favorite_book(title):
+	print(f'One of my favorite books is {title}.')
+
+favorite_book('Soldier X')
